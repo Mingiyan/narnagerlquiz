3bebbe6f78ed3e5f919ae3e0cf8df533f6953fd6
mingiyandordzhiev <vvvdmingiyan@gmail.com>
2020-05-29 19:13:58 +0300

    edit size answers

--- src/main/java/ru/halmg/narnagerl/service/button/AnswerButtonsService.java
+++ src/main/java/ru/halmg/narnagerl/service/button/AnswerButtonsService.java
@@ -9,22 +9,18 @@ import ru.halmg.narnagerl.model.Question;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AnswerButtonsService {
 
     public InlineKeyboardMarkup buildQuestion(Question question) {
         InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
-        List<List<InlineKeyboardButton>> listButtons = new ArrayList<>();
-        for (Answer answer : question.getAnswers()) {
-            List<InlineKeyboardButton> keyboardButtons = new ArrayList<>();
-            InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton().setText(answer.getAnswer()).setCallbackData(answer.getAnswer());
-            keyboardButtons.add(inlineKeyboardButton);
-            listButtons.add(keyboardButtons);
-        }
-
-        Collections.shuffle(listButtons);
-        inlineKeyboardMarkup.setKeyboard(listButtons);
+        List<InlineKeyboardButton> keyboardButtons = question.getAnswers()
+                .stream().map(answer -> new InlineKeyboardButton().setText(answer.getAnswer()).setCallbackData(answer.getAnswer()))
+                .collect(Collectors.toList());
+        Collections.shuffle(keyboardButtons);
+        inlineKeyboardMarkup.setKeyboard(Collections.singletonList(keyboardButtons));
 
         return inlineKeyboardMarkup;
     }
